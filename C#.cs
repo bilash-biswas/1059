e69796ac6a8343537d8e8725d1a27d6039497ea0
bilash-biswas <67077735+bilash-biswas@users.noreply.github.com>
2020-11-16 15:09:51 +0600

    Create C#.cs

---
+++ C#.cs
@@ -0,0 +1,16 @@
+using System.IO;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+    int i = 2;
+    while(i <= 100)
+    {
+        Console.WriteLine(i);
+        i = i + 2;
+    }
+    Console.ReadLine();
+  }
+}
