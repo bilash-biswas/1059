36d1f921a6d4761477e86f04735a8f950fdbbf43
bilash-biswas <67077735+bilash-biswas@users.noreply.github.com>
2020-11-16 15:16:35 +0600

    Create Java.java

---
+++ Java.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+     int a,b,i;
+     Scanner input = new Scanner(System.in);
+     for(i = 2;i <= 100;i = i + 2)
+     {
+         System.out.printf("%d\n", i);
+     }
+   }  
+}
